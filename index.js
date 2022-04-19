d366482a3751cea3655bac42cdc0f4a44686592e
kurigaki <kenji.kurigaki@gmail.com>
2022-04-19 23:24:49 +0900

    ページの読み込み後にJavaScriptのコードが実行されるように実装

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-const pullDownButton = document.getElementById("lists")
+window.addEventListener('load', function(){
+
+  const pullDownButton = document.getElementById("lists")
+  console.log(pullDownButton)
+})
